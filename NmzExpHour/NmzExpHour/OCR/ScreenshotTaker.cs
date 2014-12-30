15289a069b34fad2aeb902ce675b3f8310b6b965
Maxime Péloquin <maxime.peloquin@gmail.com>
2014-12-30 17:33:00 -0500

    Takes a screen shot of the screen and saves it + tests

---
+++ NmzExpHour/NmzExpHour/OCR/ScreenshotTaker.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows.Forms;
+
+namespace NmzExpHour.OCR
+{
+    public class ScreenshotTaker
+    {
+        public void TakeScreenShot(String name)
+        {
+            var bmpScreenshot = new Bitmap(SystemInformation.VirtualScreen.Width,
+                                           SystemInformation.VirtualScreen.Height);
+
+            var gfxScreenshot = Graphics.FromImage(bmpScreenshot);
+
+            gfxScreenshot.CopyFromScreen(SystemInformation.VirtualScreen.X,
+                                        SystemInformation.VirtualScreen.Y,
+                                        0,
+                                        0,
+                                        SystemInformation.VirtualScreen.Size);
+
+            bmpScreenshot.Save(name);
+        }
+    }
+}
---
+++ NmzExpHour/NmzExpHourTest/OCR/ScreenshotTakerTest.cs
@@ -0,0 +1,48 @@
+﻿using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using NmzExpHour.OCR;
+using NUnit.Framework;
+
+namespace NmzExpHourTest.OCR
+{
+    [TestFixture]
+    public class ScreenshotTakerTest
+    {
+        private const string FileName = "temp.jpg";
+        private ScreenshotTaker ssTaker;
+
+        [SetUp]
+        public void SetUp()
+        {
+            ssTaker = new ScreenshotTaker();
+        }
+
+        [Test]
+        public void AFileIsSavedOnTheDiskWithTheCorrectName()
+        {
+            ssTaker.TakeScreenShot(FileName);
+
+            Assert.That(File.Exists(FileName));
+        }
+
+        [Test]
+        public void TheSizeOfTheSavedPictureIsTheSameAsTheScreens()
+        {
+            ssTaker.TakeScreenShot(FileName);
+
+            Bitmap img = new Bitmap(FileName);
+
+            Assert.AreEqual(SystemInformation.VirtualScreen.Height, img.Height);
+            Assert.AreEqual(SystemInformation.VirtualScreen.Width, img.Width);
+
+            img.Dispose();
+        }
+
+        [TearDown]
+        public void RemoveTestFile()
+        {
+            //File.Delete(FileName);
+        }
+    }
+}
