25b5d9e1aabfd0bb50e62a396f01065c2c131095
Maxime Péloquin <maxime.peloquin@gmail.com>
2014-12-31 16:30:14 -0500

    Finds the coordinate of a color in a picture.

---
+++ NmzExpHour/NmzExpHour/OCR/ImageProcessing/ColorFinder.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+
+namespace NmzExpHour.OCR.ImageProcessing
+{
+    public class ColorFinder
+    {
+        public Point FindFirstColor(Bitmap img, Color color)
+        {
+            List<Point> listPoints = FindPoints(img, color);
+
+            return listPoints.Count != 0 ? listPoints.First() : new Point(-1, -1);
+        }
+
+        public Point FindLastColor(Bitmap img, Color color)
+        {
+            List<Point> listPoints = FindPoints(img, color);
+
+            return listPoints.Count != 0 ? listPoints.Last() : new Point(-1, -1);
+        }
+
+        public List<Point> FindPoints(Bitmap img, Color color)
+        {
+            List<Point> listPoints = new List<Point>();
+
+            for (int i = 0; i < img.Width; i++)
+            {
+                for (int j = 0; j < img.Height; j++)
+                {
+                    if (img.GetPixel(i, j).Equals(color))
+                    {
+                        listPoints.Add(new Point(i,j));
+                    }
+                }
+            }
+
+            return listPoints;
+        }
+    }
+}
---
+++ NmzExpHour/NmzExpHourTest/OCR/ImageProcessing/ColorFinderTest.cs
@@ -0,0 +1,65 @@
+﻿using System.Drawing;
+using NmzExpHour.OCR.ImageProcessing;
+using NmzExpHourTest.Data;
+using NUnit.Framework;
+
+namespace NmzExpHourTest.OCR.ImageProcessing
+{
+    [TestFixture]
+    public class ColorFinderTest
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            ColorFinder = new ColorFinder();
+        }
+
+        [Test]
+        public void ReturnsMinus1IfFirstColorNotFound()
+        {
+            Bitmap img = new Bitmap(10, 10);
+
+            Point actual = ColorFinder.FindFirstColor(img, Color.Red);
+
+            Assert.AreEqual(new Point(-1, -1), actual);
+        }
+
+        [Test]
+        public void ReturnMinus1IfLastColorNotFound()
+        {
+            Bitmap img = new Bitmap(10, 10);
+
+            Point actual = ColorFinder.FindLastColor(img, Color.Red);
+
+            Assert.AreEqual(new Point(-1, -1), actual);
+        }
+
+        [Test]
+        public void ReturnsTheLastPixelLocationOfTheColor()
+        {
+            Point expected = new Point(2558, 332);
+            Bitmap img = new Bitmap(Images.TestPointsPresent0);
+            Color color = Color.FromArgb(127, 70, 15);
+
+            Point actual = ColorFinder.FindLastColor(img, color);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [Test]
+        public void ReturnsTheFirstPixelLocationOfTheColor()
+        {
+            Point expected = new Point(2516, 303);
+            Bitmap img = new Bitmap(Images.TestPointsPresent0);
+            Color color = Color.FromArgb(127, 70, 15);
+           
+            Point actual = ColorFinder.FindFirstColor(img, color);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        public ColorFinder ColorFinder { get; set; }
+
+    }
+}
