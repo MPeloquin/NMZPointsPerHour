d3035d7be32f78f2522bd070a4650051d2f4f271
Maxime Péloquin <maxime.peloquin@gmail.com>
2014-12-31 16:31:07 -0500

    Extract the nmz points from a picture

---
+++ NmzExpHour/NmzExpHour/OCR/ImageProcessing/NMZPointsPictureFinder.cs
@@ -0,0 +1,19 @@
+﻿using System.Drawing;
+
+namespace NmzExpHour.OCR.ImageProcessing
+{
+    public interface INMZPointsPictureFinder
+    {
+        Bitmap FindPoints(Bitmap img);
+    }
+
+    public class NMZInmzPointsPicturePictureFinder : INMZPointsPictureFinder
+    {
+        public Bitmap FindPoints(Bitmap img)
+        {
+            return new Bitmap(1,1);
+        }
+
+
+    }
+}
---
+++ NmzExpHour/NmzExpHourTest/OCR/ImageProcessing/NMZPointsPictureFinderTest.cs
@@ -0,0 +1,42 @@
+﻿using System.Drawing;
+using NmzExpHour.OCR.ImageProcessing;
+using NmzExpHourTest.Data;
+using NUnit.Framework;
+
+namespace NmzExpHourTest.OCR.ImageProcessing
+{
+    [TestFixture]
+    public class NMZPointsPictureFinderTest
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            NMZInmzPointsPicturePictureFinder = new NMZInmzPointsPicturePictureFinder();
+        }
+
+
+        [Test]
+        public void FindsPointsWhenPresent()
+        {
+            var entryImage = new Bitmap(Images.TestPointsPresent0);
+            var expected = new Bitmap(Images.TestPointsPresent0Result);
+
+            var actual = NMZInmzPointsPicturePictureFinder.FindPoints(entryImage);
+
+            Assert.AreEqual(expected.Height, actual.Height);
+            Assert.AreEqual(expected.Width, actual.Width);
+
+            for (int i = 0; i < expected.Height; i++)
+            {
+                for (int j = 0; j < expected.Width; j++)
+                {
+                    Assert.AreEqual(expected.GetPixel(i,j), actual.GetPixel(i, j));
+                }
+            }
+        }
+
+
+        public NMZInmzPointsPicturePictureFinder NMZInmzPointsPicturePictureFinder { get; set; }
+
+    }
+}
