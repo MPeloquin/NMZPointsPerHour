60055bd37aec7c1bcc0a559f52cd208a46878777
Maxime Péloquin <maxime.peloquin@gmail.com>
2014-12-31 16:32:44 -0500

    Class that extracts the NMZ points from the screen and returns the points in a string form.

---
+++ NmzExpHour/NmzExpHour/OCR/NMZPointsScreenReader.cs
@@ -0,0 +1,26 @@
+﻿using NmzExpHour.OCR.ImageProcessing;
+
+namespace NmzExpHour.OCR
+{
+    public class NMZPointsScreenReader
+    {
+
+        public NMZPointsScreenReader()
+        {
+            ScreenShotTaker = new ScreenshotTaker();
+            InmzPointsPictureFinder = new NMZInmzPointsPicturePictureFinder();
+        }
+
+        public string ScreenToNMZPoints()
+        {
+            var screenshot = ScreenShotTaker.TakeScreenShot();
+            InmzPointsPictureFinder.FindPoints(screenshot);
+
+            return "";
+        }
+
+
+        public IScreenshotTaker ScreenShotTaker { get; set; }
+        public INMZPointsPictureFinder InmzPointsPictureFinder { get; set; }
+    }
+}
---
+++ NmzExpHour/NmzExpHourTest/OCR/NMZPointsScreenReaderTest.cs
@@ -0,0 +1,59 @@
+﻿using System.Drawing;
+using NmzExpHour.OCR;
+using NmzExpHour.OCR.ImageProcessing;
+using NmzExpHourTest.Data;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace NmzExpHourTest.OCR
+{
+    [TestFixture]
+    public class NMZPointsScreenReaderTest
+    {
+        private NMZPointsScreenReader nmzPointsScreenReader;
+
+        [SetUp]
+        public void SetUp()
+        {
+            nmzPointsScreenReader = new NMZPointsScreenReader();
+        }
+
+        [Test]
+        public void CallsScreenshotTaker()
+        {
+            var ssTaker = Substitute.For<IScreenshotTaker>();
+            var pointsFinder = Substitute.For<INMZPointsPictureFinder>();
+
+            nmzPointsScreenReader.ScreenShotTaker = ssTaker;
+            nmzPointsScreenReader.InmzPointsPictureFinder = pointsFinder;
+
+            nmzPointsScreenReader.ScreenToNMZPoints();
+
+            ssTaker.Received().TakeScreenShot();
+        }
+
+        [Test]
+        public void CallsPointsFinderWithScreenShot()
+        {
+            var ssTaker = Substitute.For<IScreenshotTaker>();
+            var pointsFinder = Substitute.For<INMZPointsPictureFinder>();
+
+            Bitmap img = new Bitmap(10, 10);
+            ssTaker.TakeScreenShot().Returns(img);
+
+            nmzPointsScreenReader.ScreenShotTaker = ssTaker;
+            nmzPointsScreenReader.InmzPointsPictureFinder = pointsFinder;
+
+            nmzPointsScreenReader.ScreenToNMZPoints();
+
+            pointsFinder.Received().FindPoints(img);
+        }
+
+        [Test]
+        public void CallsOCRWithScreenShot()
+        {
+            
+
+        }
+    }
+}
