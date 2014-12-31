ec37091f264e2e34e46bd5775f90bf06639cc7a9
Maxime Péloquin <maxime.peloquin@gmail.com>
2014-12-31 15:00:27 -0500

    Static class with test images location

---
+++ NmzExpHour/NmzExpHourTest/Data/Images.cs
@@ -0,0 +1,9 @@
+﻿namespace NmzExpHourTest.Data
+{
+    public static class Images
+    {
+        public static string Empty = "testEmpty.png";
+        public static string TestPointsPresent0 = "testPointsPresent0.png";
+        public static string TestPointsPresent0Result = "testPointsPresent0Result.png";
+    }
+}
