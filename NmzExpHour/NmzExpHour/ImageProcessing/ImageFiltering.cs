02d9a8dfcebc8b81bfa3334064d4470617d4fa3a
Maxime Péloquin <maxime.peloquin@gmail.com>
2015-01-05 03:45:22 -0500

    Applied faster image processing used everywhere else to the filtering

--- NmzExpHour/NmzExpHour/ImageProcessing/ImageFiltering.cs
+++ NmzExpHour/NmzExpHour/ImageProcessing/ImageFiltering.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Imaging;
 
 namespace NmzExpHour.ImageProcessing
 {
@@ -8,16 +10,38 @@ namespace NmzExpHour.ImageProcessing
 
         public Bitmap FilterImage(Bitmap img)
         {
-            for (int y = 0; y < img.Height; y++)
+            unsafe
             {
-                for (int x = 0; x < img.Width; x++)
+                BitmapData bitmapData = img.LockBits(new Rectangle(0, 0, img.Width, img.Height), ImageLockMode.ReadWrite,
+                    img.PixelFormat);
+                int bytesPerPixel = Image.GetPixelFormatSize(img.PixelFormat)/8;
+                int widthInBytes = bitmapData.Width*bytesPerPixel;
+                byte* ptrFirstPixel = (byte*) bitmapData.Scan0;
+
+                for (int y = 0; y < bitmapData.Height; y++)
                 {
-                    if (img.GetPixel(x, y) != Colors.Font)
-                        img.SetPixel(x, y, Color.White);
-                    else
-                        img.SetPixel(x, y, Color.Black);
+                    byte* currentLine = ptrFirstPixel + (y*bitmapData.Stride);
+                    for (int x = 0; x < widthInBytes; x = x + bytesPerPixel)
+                    {
+                        Color currentColor = Color.FromArgb(currentLine[x + 2], currentLine[x + 1], currentLine[x]);
+
+                        if (currentColor != Colors.Font)
+                        {
+                            currentLine[x] = 255;
+                            currentLine[x + 1] = 255;
+                            currentLine[x + 2] = 255;
+                        }
+                        else
+                        {
+                            currentLine[x] = 0;
+                            currentLine[x + 1] = 0;
+                            currentLine[x + 2] = 0;
+                        }
+                    }
                 }
+                img.UnlockBits(bitmapData);
             }
+
             return img;
         }
     }
