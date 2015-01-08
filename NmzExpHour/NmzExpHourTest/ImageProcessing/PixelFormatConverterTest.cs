5103b4a1d3f01e942c6b6a144176fb02fe761460
Maxime Péloquin <maxime.peloquin@gmail.com>
2015-01-08 18:20:41 -0500

    Had a problem with processing 32bpp images. Not 100% what the problem was but two picture exactly the same to the pixel would give different test result depending on the pixelformat. This class changes images to 24bpp, which doesn't have any problems.

---
+++ NmzExpHour/NmzExpHourTest/ImageProcessing/PixelFormatConverterTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using NmzExpHour.ImageProcessing;
+using NmzExpHourTest.Data;
+using NUnit.Framework;
+
+namespace NmzExpHourTest.ImageProcessing
+{
+    [TestFixture]
+    public class PixelFormatConverterTest
+    {
+        [Test]
+        public void Converts32BppRbGto24BppRbg()
+        {
+            var img32Bpp = new Bitmap(Images.OCRRow);
+
+            Assert.AreEqual(PixelFormat.Format32bppArgb, img32Bpp.PixelFormat);
+
+            var actual = new PixelFormatConverter().To24BppRGB(img32Bpp);
+
+            Assert.AreEqual(PixelFormat.Format24bppRgb, actual.PixelFormat);
+        }
+    }
+}
