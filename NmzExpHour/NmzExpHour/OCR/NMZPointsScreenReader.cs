a3ba3f7c9950ddbd81f6a30b42fee54194c447a6
Maxime Péloquin <maxime.peloquin@gmail.com>
2015-01-08 18:27:17 -0500

    PointsScreenReader now calls everyone he needs to with tests.

--- NmzExpHour/NmzExpHour/OCR/NMZPointsScreenReader.cs
+++ NmzExpHour/NmzExpHour/OCR/NMZPointsScreenReader.cs
@@ -1,27 +1,36 @@
-﻿using NmzExpHour.ImageProcessing;
+﻿using System.Linq;
+using NmzExpHour.ImageProcessing;
 
 namespace NmzExpHour.OCR
 {
     public class NMZPointsScreenReader
     {
-
         public NMZPointsScreenReader()
         {
             ScreenShotTaker = new ScreenshotTaker();
-            InmzPointsImageFinder = new NMZPointsImageFinder();
+            NmzPointsImageFinder = new NMZPointsImageFinder();
+            ImageFilterer = new ImageFilterer();
+            Separator = new OpticalNumberSeparator();
+            OCR = new OpticalNumberRecognizer();
         }
 
         public string ScreenToNMZPoints()
         {
             var screenshot = ScreenShotTaker.TakeScreenShot();
-            InmzPointsImageFinder.FindNMZPoints(screenshot);
+            var points = NmzPointsImageFinder.FindNMZPoints(screenshot);
+            var filteredImage = ImageFilterer.FilterImage(points);
+            var listNumbers = Separator.Separate(filteredImage);
 
 
-            return "";
+            return listNumbers.Aggregate("", (current, number) => current + OCR.RecognizeNumber(number));
         }
 
 
         public IScreenshotTaker ScreenShotTaker { get; set; }
-        public INMZPointsImageFinder InmzPointsImageFinder { get; set; }
+        public INMZPointsImageFinder NmzPointsImageFinder { get; set; }
+        public IImageFilterer ImageFilterer { get; set; }
+        public IOpticalNumberSeparator Separator { get; set; }
+        public IOpticalNumberRecognizer OCR { get; set; }
+
     }
 }
--- NmzExpHour/NmzExpHourTest/OCR/NMZPointsScreenReaderTest.cs
+++ NmzExpHour/NmzExpHourTest/OCR/NMZPointsScreenReaderTest.cs
@@ -1,7 +1,7 @@
-﻿using System.Drawing;
+﻿using System.Collections.Generic;
+using System.Drawing;
 using NmzExpHour.ImageProcessing;
 using NmzExpHour.OCR;
-using NmzExpHourTest.ImageProcessing;
 using NSubstitute;
 using NUnit.Framework;
 
@@ -16,16 +16,28 @@ namespace NmzExpHourTest.OCR
         public void SetUp()
         {
             nmzPointsScreenReader = new NMZPointsScreenReader();
+
+            var ssTaker = Substitute.For<IScreenshotTaker>();
+            var pointsFinder = Substitute.For<INMZPointsImageFinder>();
+            var ocr = Substitute.For<IOpticalNumberRecognizer>();
+            var imageFilterer = Substitute.For<IImageFilterer>();
+            var separator = Substitute.For<IOpticalNumberSeparator>();
+
+            separator.Separate(new Bitmap(1, 1)).ReturnsForAnyArgs(new List<Bitmap>());
+            
+
+            nmzPointsScreenReader.ScreenShotTaker = ssTaker;
+            nmzPointsScreenReader.NmzPointsImageFinder = pointsFinder;
+            nmzPointsScreenReader.OCR = ocr;
+            nmzPointsScreenReader.ImageFilterer = imageFilterer;
+            nmzPointsScreenReader.Separator = separator;
         }
 
         [Test]
         public void CallsScreenshotTaker()
         {
             var ssTaker = Substitute.For<IScreenshotTaker>();
-            var pointsFinder = Substitute.For<INMZPointsImageFinder>();
-
             nmzPointsScreenReader.ScreenShotTaker = ssTaker;
-            nmzPointsScreenReader.InmzPointsImageFinder = pointsFinder;
 
             nmzPointsScreenReader.ScreenToNMZPoints();
 
@@ -37,25 +49,88 @@ namespace NmzExpHourTest.OCR
         {
             var ssTaker = Substitute.For<IScreenshotTaker>();
             var pointsFinder = Substitute.For<INMZPointsImageFinder>();
+            nmzPointsScreenReader.ScreenShotTaker = ssTaker;
+            nmzPointsScreenReader.NmzPointsImageFinder = pointsFinder;
 
             Bitmap img = new Bitmap(10, 10);
             ssTaker.TakeScreenShot().Returns(img);
 
-            nmzPointsScreenReader.ScreenShotTaker = ssTaker;
-            nmzPointsScreenReader.InmzPointsImageFinder = pointsFinder;
-
             nmzPointsScreenReader.ScreenToNMZPoints();
 
             pointsFinder.Received().FindNMZPoints(img);
-
-            img.Dispose();
         }
 
         [Test]
-        public void CallsOCRWithScreenShot()
+        public void CallsImageFiltererWithPoints()
         {
-            
+            var pointsFinder = Substitute.For<INMZPointsImageFinder>();
+            var imageFilterer = Substitute.For<IImageFilterer>();
+            nmzPointsScreenReader.NmzPointsImageFinder = pointsFinder;
+            nmzPointsScreenReader.ImageFilterer = imageFilterer;
 
+            Bitmap img = new Bitmap(10, 10);
+            pointsFinder.FindNMZPoints(new Bitmap(1, 1)).ReturnsForAnyArgs(img);
+            
+            nmzPointsScreenReader.ScreenToNMZPoints();
+
+            imageFilterer.Received().FilterImage(img);
+        }
+        
+        [Test]
+        public void CallsNumberSeparatorWithFilteredImage()
+        {
+            var separator = Substitute.For<IOpticalNumberSeparator>();
+            var imageFilterer = Substitute.For<IImageFilterer>();
+            nmzPointsScreenReader.Separator = separator;
+            nmzPointsScreenReader.ImageFilterer = imageFilterer;
+
+            Bitmap img = new Bitmap(10, 10);
+            imageFilterer.FilterImage(new Bitmap(1, 1)).ReturnsForAnyArgs(img);
+            separator.Separate(new Bitmap(1, 1)).ReturnsForAnyArgs(new List<Bitmap>());
+            
+            nmzPointsScreenReader.ScreenToNMZPoints();
+
+            separator.Received().Separate(img);
+        }
+
+        [Test]
+        public void CallsOCROnEachNumbers()
+        {
+            var separator = Substitute.For<IOpticalNumberSeparator>();
+            var ocr = Substitute.For<IOpticalNumberRecognizer>(); 
+            nmzPointsScreenReader.Separator = separator;
+            nmzPointsScreenReader.OCR = ocr;
+
+            Bitmap img1 = new Bitmap(2,2);
+            Bitmap img2 = new Bitmap(2,2);
+            List<Bitmap> imgs = new List<Bitmap> { img1, img2 };
+            separator.Separate(new Bitmap(1, 1)).ReturnsForAnyArgs(imgs);
+
+            nmzPointsScreenReader.ScreenToNMZPoints();
+
+            ocr.Received().RecognizeNumber(img1);
+            ocr.Received().RecognizeNumber(img2);
+        }
+
+        [Test]
+        public void ReturnsNumbersFoundByOCR()
+        {
+            var separator = Substitute.For<IOpticalNumberSeparator>();
+            var ocr = Substitute.For<IOpticalNumberRecognizer>();
+            nmzPointsScreenReader.Separator = separator;
+            nmzPointsScreenReader.OCR = ocr;
+
+            Bitmap img1 = new Bitmap(2, 2);
+            Bitmap img2 = new Bitmap(2, 2);
+            List<Bitmap> imgs = new List<Bitmap> { img1, img2 };
+            separator.Separate(new Bitmap(1, 1)).ReturnsForAnyArgs(imgs);
+
+            ocr.RecognizeNumber(img1).Returns("1");
+            ocr.RecognizeNumber(img2).Returns("2");
+
+            var actual = nmzPointsScreenReader.ScreenToNMZPoints();
+
+            Assert.AreEqual("12", actual);
         }
     }
 }
