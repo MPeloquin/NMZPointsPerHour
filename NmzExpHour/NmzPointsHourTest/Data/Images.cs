6a5eb73a91040510d6018d26a7763f6c4849f0e7
Maxime Péloquin <maxime.peloquin@gmail.com>
2015-09-02 20:53:44 -0400

    Moved test images to their own dir

--- NmzExpHour/NmzPointsHourTest/Data/Images.cs
+++ NmzExpHour/NmzPointsHourTest/Data/Images.cs
@@ -2,36 +2,36 @@
 {
     public static class Images
     {
-        public static string PointsFullImage = "Images\\testPointsPresent0.png";
-        public static string PointsFullImageResizable = "Images\\TestPresentResizable.png";
-        public static string PointsSmallImage = "Images\\testPointsPresent0Result.png";
-        public static string PointsFullImageResizableResult = "Images\\TestPresentResizableResult.png";
-        public static string NoPoints = "Images\\testPointsAbsent.png";
-        public static string LastPixel = "Images\\lastpixel.png";
-        public static string PointsText = "Images\\pointsText.png";
-        public static string NumbersNoPoints = "Images\\textNoPoints.png";
-        public static string TwoCommas = "Images\\TwoCommas.png";
-        public static string TwoCommasNoComma = "Images\\TwoCommasNoComma.png";
-        public static string Ones = "Images\\Ones.png";
-        public static string Image32Bpp = "Images\\32bppImage.png";
+        private static readonly string FolderImages = "..//..//Testfiles//Images//";
+        public static string PointsFullImage = FolderImages + "testPointsPresent0.png";
+        public static string PointsFullImageResizable = FolderImages + "TestPresentResizable.png";
+        public static string PointsSmallImage = FolderImages + "testPointsPresent0Result.png";
+        public static string PointsFullImageResizableResult = FolderImages + "TestPresentResizableResult.png";
+        public static string NoPoints = FolderImages + "testPointsAbsent.png";
+        public static string LastPixel = FolderImages + "lastpixel.png";
+        public static string PointsText = FolderImages + "pointsText.png";
+        public static string NumbersNoPoints = FolderImages + "textNoPoints.png";
+        public static string TwoCommas = FolderImages + "TwoCommas.png";
+        public static string TwoCommasNoComma = FolderImages + "TwoCommasNoComma.png";
+        public static string Ones = FolderImages + "Ones.png";
+        public static string Image32Bpp = FolderImages + "32bppImage.png";
 
-
-        public static string OCROne = "OCRImages\\1.jpg";
-        public static string OCRTwo = "OCRImages\\2.jpg";
-        public static string OCRThree = "OCRImages\\3.jpg";
-        public static string OCRFour = "OCRImages\\4.jpg";
-        public static string OCRFive = "OCRImages\\5.jpg";
-        public static string OCRSix = "OCRImages\\6.jpg";
-        public static string OCRSeven = "OCRImages\\7.jpg";
-        public static string OCRHeigh = "OCRImages\\8.jpg";
-        public static string OCRNine = "OCRImages\\9.jpg";
-        public static string OCRZero = "OCRImages\\0.jpg";
-        public static string OCRRow = "OCRImages\\RowNumbers.jpg";
-        public static string OCRRowComma = "OCRImages\\RowNumbersComma.jpg";
-        public static string OCRLarge9 = "OCRImages\\Large9.jpg";
-        public static string OCRTwoNumbers = "OCRImages\\TwoNumbers.jpg";
-        public static string OCRThreeNumbers = "OCRImages\\ThreeNumbers.jpg";
-
-
+        private static readonly string FolderOCRImages = "..//..//Testfiles//OCRImages//";
+        public static string OCROne = FolderOCRImages + "1.jpg";
+        public static string OCRTwo = FolderOCRImages + "2.jpg";
+        public static string OCRThree = FolderOCRImages + "3.jpg";
+        public static string OCRFour = FolderOCRImages + "4.jpg";
+        public static string OCRFive = FolderOCRImages + "5.jpg";
+        public static string OCRSix = FolderOCRImages + "6.jpg";
+        public static string OCRSeven = FolderOCRImages + "7.jpg";
+        public static string OCRHeigh = FolderOCRImages + "8.jpg";
+        public static string OCRNine = FolderOCRImages + "9.jpg";
+        public static string OCRZero = FolderOCRImages + "0.jpg";
+        public static string OCRRow = FolderOCRImages + "RowNumbers.jpg";
+        public static string OCRRowComma = FolderOCRImages + "RowNumbersComma.jpg";
+        public static string OCRLarge9 = FolderOCRImages + "Large9.jpg";
+        public static string OCRTwoNumbers = FolderOCRImages + "TwoNumbers.jpg";
+        public static string OCRThreeNumbers = FolderOCRImages + "ThreeNumbers.jpg";
+        public static string OCRNoNumber = FolderOCRImages + "noNumber.jpg";
     }
 }
